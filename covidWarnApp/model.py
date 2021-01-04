2d02c62271e03d99699ce927bea21c8d4cdd7a1f
Felix Reyero <felixreyero@gmail.com>
2021-01-04 20:35:35 -0300

    some changes

--- covidWarnApp/model.py
+++ covidWarnApp/model.py
@@ -5,11 +5,11 @@ from flask import current_app
 from covidWarnApp.database import db
 
 
-class Users(db.Model):
+class Nations(db.Model):
     # __tablename__= 'users'
 
     id_user = db.Column(db.Integer, primary_key=True)
     alias = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
-        return f"User: {self.alias}"
+        return f"Nation: {self.alias}"
