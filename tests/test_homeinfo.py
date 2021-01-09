bab48cfc2e5d4e9af957748a0e9a53ccd296f168
Felix Reyero <felixreyero@gmail.com>
2021-01-09 16:05:59 -0300

    test fix

---
+++ tests/test_homeinfo.py
@@ -0,0 +1,23 @@
+import json
+from covidWarnApp import create_app
+import unittest
+
+
+class FlaskTest(unittest.TestCase):
+
+    def test_index(self):
+        tester = create_app().test_client(self)
+        response = tester.get("/")
+        status_code = response.status_code
+
+        # Check for response 200
+        self.assertEqual(status_code, 200)
+
+    def test_health(self):
+        tester = create_app().test_client(self)
+        response = tester.get("/health")
+        status_code = response.status_code
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(data['status'], "UP")
+        # Check for response 200
+        self.assertEqual(status_code, 200)
